369b6b74936c45ab4f691fb778dbc5d33af4d8c0
Littlearphone <littlearphone@outlook.com>
2019-03-06 16:20:07 +0800

    Update steps_file.js

--- steps_file.js
+++ steps_file.js
@@ -1,7 +1,31 @@
-// in this file you can append custom step methods to 'I' object
+/****************************************************************************************************************************************************
+ *                                                                                                                                                  *
+ *  * Copyright: https://github.com/Littlearphone                                                                                                   *
+ *                                                                                                                                                  *
+ ****************************************************************************************************************************************************/
+const common = require("./tests/data/000_common.js");
 module.exports = function () {
     return actor({
-        // Define custom steps here, use 'this' to access default methods of I.
-        // It is recommended to place a general 'login' function here.
+        loginAdmin() {
+            this.amOnPage(common.baseUrl);
+            this.fillField("#username", common.username);
+            this.fillField("#password", common.password);
+            this.click("Login");
+        },
+        gotoModule(module, frame, frameCallback) {
+            this.waitForText("Admin", common.defaultTimeout, ".banner");
+            this.waitForElement(module, common.defaultTimeout);
+            this.click(module);
+            if (frame) {
+                this.waitForElement(frame, common.defaultTimeout);
+                // this.wait(common.queryTimeout);
+                within({frame: frame}, frameCallback);
+            }
+        },
+        searchGrid(keyword, input, button) {
+            this.seeElement(input, common.defaultTimeout);
+            this.fillField(input, keyword);
+            this.click(button);
+        }
     });
 };
