e9b1ead9389111f5fd66c842c90d87db6814ef78
Littlearphone <littlearphone@outlook.com>
2019-03-06 16:18:07 +0800

    Update codecept.conf.js

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,12 +1,52 @@
+/****************************************************************************************************************************************************
+ *                                                                                                                                                  *
+ *  * Copyright: https://github.com/Littlearphone                                                                                                   *
+ *                                                                                                                                                  *
+ ****************************************************************************************************************************************************/
+const common = require("./tests/data/000_common.js");
+const webDriver = {
+    "chrome": {
+        browser: "chrome",
+        desiredCapabilities: {
+            "chromeOptions": {
+                "args": [
+                    // "--headless",
+                    "--disable-gpu"
+                ]
+            }
+        }
+    },
+    "ie": {
+        browser: "internet explorer",
+        desiredCapabilities: {
+            "ieOptions": {
+                "ie.browserCommandLineSwitches": "-private",
+                "ie.usePerProcessProxy": true,
+                "ie.ensureCleanSession": false
+            }
+        }
+    }
+};
 exports.config = {
-    tests: './tests/*_test.js',
+    tests: './tests/modules/**/*_test.js',
     output: './output',
     helpers: {
+        // Nightmare: {
+        //     url: "http://localhost",
+        //     windowSize: "1920x1024",
+        //     show: true,
+        //     restart: false
+        // },
         // Puppeteer: {
         //     url: 'http://localhost',
+        //     windowSize: "1920x1024",
+        //     timeouts: {
+        //         "script": 10000,
+        //         "page load": 10000
+        //     },
         //     show: true
         // },
-        WebDriver: {
+        WebDriver: Object.assign({
             host: "localhost",
             url: "http://localhost",
             smartWait: 5000,
@@ -14,20 +54,39 @@ exports.config = {
             restart: false,
             windowSize: "maximize",
             timeouts: {
-                "script": 60000,
+                "script": 10000,
                 "page load": 10000
             },
-            browser: "internet explorer",
-            desiredCapabilities: {
-                "ieOptions": {
-                    "ie.browserCommandLineSwitches": "-private",
-                    "ie.usePerProcessProxy": true,
-                    "ie.ensureCleanSession": false
+            keepCookies: true  // keep cookies for all tests
+        }, webDriver.chrome),
+        conditional: {
+            require: './helpers/conditional_helper.js'
+        },
+        jsFailure: {
+            require: './helpers/js_failure_helper.js'
+        },
+        jsWait: {
+            require: './helpers/js_wait_helper.js'
+        }
+    },
+    plugins: {
+        autoLogin: {
+            enabled: true,
+            saveToFile: false,
+            inject: 'login',
+            users: {
+                admin: {
+                    login: (I) => I.loginAdmin(),
+                    check: (I) => I.waitForText("Admin", common.defaultTimeout, ".banner"),
+                    restore: (I) => I.amOnPage(common.baseUrl)
                 }
             }
         },
-        conditional: {
-            require: './helpers/conditional_helper.js'
+        autoDelay: {
+            "enabled": true
+        },
+        retryFailedStep: {
+            "enabled": true
         }
     },
     include: {
